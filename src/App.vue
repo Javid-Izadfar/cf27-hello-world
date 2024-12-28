5ad73210a849e83dc29c16967663dacef640ddac
Javid Izadfar <javid.izadfar@gmail.com>
2024-12-28 21:41:42 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const name = ref("");
+</script>
+
+<template>
+  <div class="hi">
+    <input v-model="name" type="text" />
+    <div>hi {{ name }}</div>
+    <HelloWorld />
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hi">Inside HelloWorld.vue</div>
+</template>
+
+<style scoped>
+.hi {
+  border: solid 1px red;
+}
+</style>
