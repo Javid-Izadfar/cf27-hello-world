d0ee5a0e54d726e87c82ec678b5c55beda5d0971
Javid Izadfar <javid.izadfar@gmail.com>
2024-12-31 21:46:17 +0330

    added vue conditions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
-import { ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-const name = ref("");
+import CompA from "@/components/CompA.vue";
+import CompB from "@/components/CompB.vue";
 </script>
-
 <template>
-  <div class="hi">
-    <input v-model="name" type="text" />
-    <div>hi {{ name }}</div>
-    <HelloWorld />
-  </div>
+  <div>App.vue</div>
+  <CompA />
+  <!-- <CompB /> -->
 </template>
--- src/components/HelloWorld.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="hi">Inside HelloWorld.vue</div>
-</template>
-
-<style scoped>
-.hi {
-  border: solid 1px red;
-}
-</style>
