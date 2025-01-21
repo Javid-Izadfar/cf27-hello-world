1043e24da78b4457078d120ed6b1b2d3e8fb6cd4
Javid Izadfar <javid.izadfar@gmail.com>
2025-01-21 18:31:07 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-import CompA from "@/components/old/CompA.vue";
-import CompB from "@/components/old/CompB.vue";
-</script>
 <template>
-  <div>App.vue</div>
+  <div>hi</div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import "./assets/main.css";
 
 import App from "./App.vue";
 
-// import Vue from 'vue'
-// const { createApp } = Vue
 import { createApp } from "vue";
 
 createApp(App).mount("#app");
